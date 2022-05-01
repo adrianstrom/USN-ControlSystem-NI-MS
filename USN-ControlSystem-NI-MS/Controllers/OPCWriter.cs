ef7ac8c13387be892268573c6bb602c8336b3772
Adrian Strom <nostroma@krohne.com>
2022-05-01 13:33:56 +0200

    added server

--- USN-ControlSystem-NI-MS/Controllers/OPCWriter.cs
+++ USN-ControlSystem-NI-MS/Controllers/OPCWriter.cs
@@ -1,7 +1,25 @@
-﻿namespace USN_ControlSystem_NI_MS.Controllers
-{
-    public class OPCWriter
-    {
+﻿using Opc.UaFx.Client;
 
+namespace USN_ControlSystem_NI_MS.Controllers
+{
+    public class OPCClient
+    {
+        private OpcClient _client;
+
+        public OPCClient(string hostName)
+        {
+            _client = new OpcClient(hostName);
+        }
+
+        public void WriteValues()
+        {
+            using (var client = new OpcClient("opc.tcp://localhost:4840"))
+            {
+                client.Connect();
+
+                var temperature = client.ReadNode("ns=2;s=Temperature");
+                //var controlSignal = client.ReadNode("ns=2;s=ControlSignal");
+            }
+        }
     }
 }
---
+++ USN-OPC-UA-Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Server;
+using System.Threading;
+
+namespace USN_OPC_UA_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = new OpcFolderNode("USN");
+            var temperature = new OpcDataVariableNode<double>(parent, "temperature");
+
+            using (var server = new OpcServer("opc.tcp://localhost:4840/", parent))
+            {
+                server.Start();
+
+                while (true)
+                {
+                    parent.ApplyChanges(server.SystemContext);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
