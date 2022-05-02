7bb16cd899212901258febeac0af38cfe0a310a3
Adrian Strom <nostroma@krohne.com>
2022-05-02 18:50:09 +0200

    tests

---
+++ USN-ControlSystem-NI-MS.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace USN_ControlSystem_NI_MS.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
